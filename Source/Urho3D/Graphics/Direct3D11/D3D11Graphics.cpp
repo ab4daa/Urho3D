921a39f3333bfd241afe8d65c2853d58ebad7a84
ab4daa <ab4daa@gmail.com>
2019-05-01 15:06:31 +0800

    temporary fix bool shader parameter

--- Source/Urho3D/Graphics/Direct3D11/D3D11Graphics.cpp
+++ Source/Urho3D/Graphics/Direct3D11/D3D11Graphics.cpp
@@ -1071,7 +1071,8 @@ void Graphics::SetShaderParameter(StringHash param, bool value)
     ConstantBuffer* buffer = i->second_.bufferPtr_;
     if (!buffer->IsDirty())
         impl_->dirtyConstantBuffers_.Push(buffer);
-    buffer->SetParameter(i->second_.offset_, sizeof(bool), &value);
+	bool data[4] = { value };
+    buffer->SetParameter(i->second_.offset_, sizeof(data), data);
 }
 
 void Graphics::SetShaderParameter(StringHash param, const Color& color)
